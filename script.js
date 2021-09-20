72b56538a8f0a6a1fd39353c3c111b876d97f341
BenW00 <ispyb2000@gmail.com>
2021-09-20 19:49:33 -0400

    finished exercise

---
+++ script.js
@@ -0,0 +1,37 @@
+console.log(21)
+
+// Strings, Numbers, Booleans, Null, and Undefined.
+
+// -, +, **, %, *, and /
+
+console.log(5%2)
+
+console.log(5%4)
+
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+
+console.log("Hawk" + "eye");
+
+console.log ("Ben".length + "Wright".length)
+
+console.log("Avengers: Endgame"[16]);
+
+console.log("\\")
+
+console.log(5**4-9)
+
+console.log("I".length + "Am".length % "Iron".length - "Man".length)
+
+console.log("CAP"[1])
+console.log("VISION"[0])
+console.log("OKOYE"[4])
+console.log("NICK FURY"[0])
+console.log("GROOT"[0])
+console.log("DOCTOR STRANGE"[13])
+console.log("THOR"[3])
+console.log("STAR-LORD"[0])
+
+var m = 25
+var c = 4
+var u = m+c
+console.log(u)
